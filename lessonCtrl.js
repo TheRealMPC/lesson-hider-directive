4be953861542dea205846bacedbf87f6fef5bb1e
TheRealMPC <smkscar25@hotmail.com>
2017-07-25 16:24:37 -0500

    Fixed announceDay alert

--- lessonCtrl.js
+++ lessonCtrl.js
@@ -2,6 +2,10 @@ angular.module('directivePractice').controller('lessonCtrl', function ($scope){
   $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
   $scope.test = 'Two-way data binding!';
   $scope.announceDay = function(lesson, day){
+    if (!day) {
+      alert(lesson + ' is not currently scheduled.');
+    } else {
     alert(lesson + ' is active on ' + day + '.');
+    }
   };
 });
